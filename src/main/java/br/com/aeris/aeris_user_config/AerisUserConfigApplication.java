8ff18ddf5a31e97c63df88f333c3844e8138fd0c
Maria Clara Fadel <mariaclarapereira@alunos.utfpr.edu.br>
2025-10-24 23:09:13 -0300

    initializr spring boot microservice

---
+++ src/main/java/br/com/aeris/aeris_user_config/AerisUserConfigApplication.java
@@ -0,0 +1,13 @@
+package br.com.aeris.aeris_user_config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AerisUserConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AerisUserConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/aeris/aeris_user_config/AerisUserConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aeris.aeris_user_config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AerisUserConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
